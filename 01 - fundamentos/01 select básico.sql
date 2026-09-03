15ed223a55ab5059d947a7a40295f8da9199ebe8
Kyssana Ellen <kyssana.silva@gmail.com>
2026-09-03 12:16:53 -0300

    primeiro desafio

---
+++ 01 - fundamentos/01 select básico.sql
@@ -0,0 +1,5 @@
+Pergunta de negócio que responde
+
+Quem são os primeiros 20 clientes cadastrados e quais são suas informações básicas de localização?
+
+SELECT id_cliente, nome, cidade, estado FROM clientes LIMIT 20
