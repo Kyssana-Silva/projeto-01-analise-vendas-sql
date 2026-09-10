d57244930848516ec9872e46182879cbfa1416b6
Kyssana Ellen <kyssana.silva@gmail.com>
2026-09-10 19:34:41 -0300

    desafio com CTE

---
+++ 05- subconsultas-e-CTE/03 - CTE.sql
@@ -0,0 +1,15 @@
+WITH vendas_filtradas AS ( 
+SELECT v.data_venda,
+     i.quantidade,
+     i.preco_unitario,
+     i.desconto 
+FROM vendas v JOIN itens_venda i ON v.id_venda = i.id_venda 
+WHERE v.data_venda BETWEEN '2025-01-01' 
+    AND '2025-12-31' 
+    AND v.status='Concluída')
+    
+SELECT to_char(data_venda,'MM/YYYY') AS mes,
+     SUM ((quantidade*preco_unitario)-desconto) AS faturamento_mensal 
+FROM vendas_filtradas 
+GROUP BY date_trunc('month', data_venda),
+     to_char(data_venda, 'MM/YYYY')
